451d97fee42fcd6a5198247dee635dfca58613f3
HAGEN Tore <tore.hagen@soprasteria.com>
2024-01-09 10:35:26 +0100

    initial version

---
+++ src/main/java/tore/springboot/bookconsumer/BookConsumerApplication.java
@@ -0,0 +1,13 @@
+package tore.springboot.bookconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tore/springboot/bookconsumer/BookConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package tore.springboot.bookconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
